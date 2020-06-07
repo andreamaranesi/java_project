ade2e0d613ab3fdcb44aee56c6cff788cc3a3aed
Alessio <s1089840@studenti.univpm.it>
2020-06-07 15:18:10 +0200

    creazione classe credenziali_utenti

---
+++ instagram_api/src/main/java/com/instagram/api/config_generali/credenziali_utenti.java
@@ -0,0 +1,14 @@
+package com.instagram.api.config_generali;
+ 
+public class credenziali_utenti {
+
+	String access_token;
+
+	public String getAccess_token() {
+		return access_token;
+	}
+
+	public void setAccess_token(String access_token) {
+		this.access_token = access_token;
+	}
+}
