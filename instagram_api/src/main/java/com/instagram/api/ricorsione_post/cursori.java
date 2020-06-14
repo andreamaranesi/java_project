f2eafce14e138e9c1f8ce97fce02be59c84af088
Alessio <s1089840@studenti.univpm.it>
2020-06-14 13:05:12 +0200

    javadoc package ricorsione_post

--- instagram_api/src/main/java/com/instagram/api/ricorsione_post/cursori.java
+++ instagram_api/src/main/java/com/instagram/api/ricorsione_post/cursori.java
@@ -3,6 +3,10 @@ package com.instagram.api.ricorsione_post;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Classe contenente l'attributo @param successivo, da utilizzare nella ricorsione per ottenere tutti i post dell'utente
+ *@see com.instagram.api.modelli.chiamate_API#ottieni_post(Object, String, long, boolean)
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class cursori {
 
--- instagram_api/src/main/java/com/instagram/api/ricorsione_post/media.java
+++ instagram_api/src/main/java/com/instagram/api/ricorsione_post/media.java
@@ -6,6 +6,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Contiene le specifiche di ogni media. Poichè {@link com.instagram.api.modelli.chiamate_API#iterazione_ottieni_media(com.instagram.api.utenti.post, String, String, int, long)} ritorna un numero limitato di post, è necessario effettuare un ciclo
+ * utilizzando @param successivo, affinchè non restituisce una lista @param id_media nulla
+ *
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class media {
 
