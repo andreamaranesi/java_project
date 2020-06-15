d1fddc5257731fcf25a1c2fd8ada5e01145aaac1
Alessio <s1089840@studenti.univpm.it>
2020-06-15 13:18:37 +0200

    javadoc

--- instagram_api/src/main/java/com/instagram/api/ricorsione_post/cursori.java
+++ instagram_api/src/main/java/com/instagram/api/ricorsione_post/cursori.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Classe contenente l'attributo <b>successivo</b>, da utilizzare nella ricorsione per ottenere tutti i post dell'utente
- *@see com.instagram.api.modelli.chiamate_API#ottieni_post(Object, String, long, boolean)
+ * @see com.instagram.api.modelli.chiamate_API#ottieni_post(Object, String, long, boolean)
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class cursori {
